42a4ad8e461cab23b22e99f10871b563a39a35d6
Leonard Moses <qalexeon@Polaris.lan>
2022-11-16 16:36:46 -0500

    Refactor. Make function for adding julian days as object property for events.

--- src/components/JulianDays.js
+++ src/components/JulianDays.js
@@ -12,63 +12,68 @@ const JulianDays = (props) => {
     // console.log(year)
     // console.log(new Date(`Jan 1, ${year}`))
 
-    let startTimeJulianDay = ''
-    let startTimeMonth = ''
-    let startTimeDay = ''
-    let startTimeMonthToJulianDayPrev = ''
 
-    // Adds julian days as new object property to events
-    for (let i=0; i<props.events.length; i++) {
-        //gets just the month number
-        startTimeMonth = props.events[i].startTime.substring(5,7);
-
-        // Checks for leap year and uses appropriate julianConverterKey
-        if (!isLeapYear) {
-            //converts the month number to julian count of last day of previous month
-            startTimeMonthToJulianDayPrev = JulianConverterKey[startTimeMonth];
-        } else {
-            startTimeMonthToJulianDayPrev = JulianConverterKeyLeap[startTimeMonth];
+    // #region Adds julian days as new object property to events
+    
+    const addJulianDaysObjectProperty = () => {
+        let startTimeJulianDay = ''
+        let startTimeMonth = ''
+        let startTimeDay = ''
+        let startTimeMonthToJulianDayPrev = ''
+        let endTimeJulianDay = ''
+        let endTimeMonth = ''
+        let endTimeDay = ''
+        let endTimeMonthToJulianDayPrev = ''
+        
+        // loops through each event to convert startTime
+        for (let i=0; i<props.events.length; i++) {
+            //gets just the month number
+            startTimeMonth = props.events[i].startTime.substring(5,7);
+    
+            // Checks for leap year and uses appropriate julianConverterKey
+            if (!isLeapYear) {
+                //converts the month number to julian count of last day of previous month
+                startTimeMonthToJulianDayPrev = JulianConverterKey[startTimeMonth];
+            } else {
+                startTimeMonthToJulianDayPrev = JulianConverterKeyLeap[startTimeMonth];
+            }
+    
+            //gets just the day number of the month
+            startTimeDay = props.events[i].startTime.substring(8,10);
+    
+            // takes last day of prev month and adds event's day of month, resulting in event's julian day
+            startTimeJulianDay = parseInt(startTimeMonthToJulianDayPrev) + parseInt(startTimeDay)
+    
+            // takes event's julian day and adds it as a new object property to event 
+            props.events[i].startTimeJulianDay = startTimeJulianDay
         }
-
-        //gets just the day number of the month
-        startTimeDay = props.events[i].startTime.substring(8,10);
-
-        // takes last day of prev month and adds event's day of month, resulting in event's julian day
-        startTimeJulianDay = parseInt(startTimeMonthToJulianDayPrev) + parseInt(startTimeDay)
-
-        // takes event's julian day and adds it as a new object property to event 
-        props.events[i].startTimeJulianDay = startTimeJulianDay
-
-        // console.log(startTimeJulianDay)
-    }
-
-    let endTimeJulianDay = ''
-    let endTimeMonth = ''
-    let endTimeDay = ''
-    let endTimeMonthToJulianDayPrev = ''
-
-    for (let i=0; i<props.events.length; i++) {
-        //gets just the month number
-        endTimeMonth = props.events[i].endTime.substring(5,7);
-
-        // Checks for leap year and uses appropriate julianConverterKey
-        if (!isLeapYear) {
-            //converts the month number to julian count of last day of previous month
-            endTimeMonthToJulianDayPrev = JulianConverterKey[endTimeMonth];
-        } else {
-            endTimeMonthToJulianDayPrev = JulianConverterKeyLeap[endTimeMonth];
+    
+        // loops through each event to convert endTime
+        for (let i=0; i<props.events.length; i++) {
+            //gets just the month number
+            endTimeMonth = props.events[i].endTime.substring(5,7);
+    
+            // Checks for leap year and uses appropriate julianConverterKey
+            if (!isLeapYear) {
+                //converts the month number to julian count of last day of previous month
+                endTimeMonthToJulianDayPrev = JulianConverterKey[endTimeMonth];
+            } else {
+                endTimeMonthToJulianDayPrev = JulianConverterKeyLeap[endTimeMonth];
+            }
+    
+            //gets just the day number of the month
+            endTimeDay = props.events[i].endTime.substring(8,10);
+    
+            // takes last day of prev month and adds event's day of month
+            endTimeJulianDay = parseInt(endTimeMonthToJulianDayPrev) + parseInt(endTimeDay)
+    
+            // takes event's julian day and adds it as a new object property to event
+            props.events[i].endTimeJulianDay = endTimeJulianDay
         }
-
-        //gets just the day number of the month
-        endTimeDay = props.events[i].endTime.substring(8,10);
-
-        // takes last day of prev month and adds event's day of month
-        endTimeJulianDay = parseInt(endTimeMonthToJulianDayPrev) + parseInt(endTimeDay)
-
-        // takes event's julian day and adds it as a new object property to event
-        props.events[i].endTimeJulianDay = endTimeJulianDay
-
     }
+    
+    addJulianDaysObjectProperty()
+    // #endregion
 
     // Creates a new event property, dateRangeJulian and pushes all the range of the start to end dates into it
     for (let i=0; i<props.events.length; i++) {
