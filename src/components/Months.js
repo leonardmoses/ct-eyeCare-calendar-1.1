eb3a2572fb66f748896c7e3ab8afdd23ed1bf670
Qalexeon <qalexeon@Xavier.lan>
2022-11-28 12:37:00 -0500

    In Months.js, swapped the a tags and div so that the a comes ahead of div for months. This allows the whole div to be clickable.

--- src/components/Months.js
+++ src/components/Months.js
@@ -8,48 +8,76 @@ const Months = (props) => {
     return ( 
 
             <div className="monthsContainer">
-                <div className="months" id="Dec" >
-                    <a href="#Jan1">Jan</a>
-                </div>
-                <div className="months" id="Jan" >
-                    <a href="#Jan1">Jan</a>
-                </div>
-                <div className="months" id="Feb" >
-                    <a href="#Feb1">Feb</a>
-                </div>
-                <div className="months" id="Mar" >
-                    <a href="#Mar1">Mar</a>
-                </div>
-                <div className="months" id="Apr" >
-                    <a href="#Apr1">Apr</a>
-                </div>
-                <div className="months" id="May" >
-                     <a href="#May1">May</a>
-                </div>
-                <div className="months" id="Jun" >
-                    <a href="#Jun1">Jun</a>
-                </div>
-                <div className="months" id="Jul" >
-                    <a href="#Jul1">Jul</a>
-                </div>
-                <div className="months" id="Aug" >
-                    <a href="#Aug1">Aug</a>
-                </div>
-                <div className="months" id="Sep" >
-                    <a href="#Sep1">Sep</a>
-                </div>
-                <div className="months" id="Oct" >
-                    <a href="#Oct1">Oct</a>
-                </div>
-                <div className="months" id="Nov" >
-                    <a href="#Nov1">Nov</a>
-                </div>
-                <div className="months" id="Dec" >
-                    <a href="#Dec1">Dec</a>
-                </div>
-                <div className="months" id="Jan" >
-                    <a href="#Dec1">Jan</a>
-                </div>
+                
+                <a href="#Jan1">
+                    <div className="months" id="Dec" >Dec</div>
+                </a>
+                
+                
+                <a href="#Jan1">
+                    <div className="months" id="Jan" >Jan</div>
+                </a>
+                
+                
+                <a href="#Feb1">
+                    <div className="months" id="Feb" >Feb</div>
+                </a>
+                
+                
+                <a href="#Mar1">
+                    <div className="months" id="Mar" >Mar</div>
+                </a>
+                
+                
+                <a href="#Apr1">
+                    <div className="months" id="Apr" >Apr</div>
+                </a>
+                
+                
+                <a href="#May1">
+                    <div className="months" id="May" >May</div>
+                </a>
+                
+                
+                <a href="#Jun1">
+                    <div className="months" id="Jun" >Jun</div>
+                </a>
+                
+                
+                <a href="#Jul1">
+                    <div className="months" id="Jul" >Jul</div>
+                </a>
+            
+            
+                <a href="#Aug1">
+                    <div className="months" id="Aug" >Aug</div>
+                </a>
+            
+            
+                <a href="#Sep1">
+                    <div className="months" id="Sep" >Sep</div>
+                </a>
+            
+            
+                <a href="#Oct1">
+                    <div className="months" id="Oct" >Oct</div>
+                </a>
+            
+            
+                <a href="#Nov1">
+                    <div className="months" id="Nov" >Nov</div>
+                </a>
+            
+            
+                <a href="#Dec1">
+                    <div className="months" id="Dec" >Dec</div>
+                </a>
+            
+            
+                <a href="#Dec1">
+                    <div className="months" id="Jan" >Jan</div>
+                </a>
+                
             </div>
      );
 }
