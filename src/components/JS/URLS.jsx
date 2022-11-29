b99e778027094a2572569d4ad604c7f40389fd65
Qalexeon <qalexeon@Xavier.lan>
2022-11-29 15:17:04 -0500

    Changed URL to heroku. Cleaned up some  files. Deleted original modal component(unused).

--- src/components/JS/URLS.jsx
+++ src/components/JS/URLS.jsx
@@ -3,19 +3,9 @@
 //  #region URLS
 export let URL = ''
 
-// URL = `https://reqres.in/api/users`
-// URL = 'https://reqres.in/api/users/2'
-// URL = "https://contrivia-backend.herokuapp.com/trivia";
-// URL = 'https://see-care-php-7-4-kevincranmer180966.codeanyapp.com/see-care-php-7.4/staff/calendar/querypage.php'
-// URL = 'https://see-care.com/caljsontest.php'
-// URL = 'https://date.nager.at/api/v2/publicholidays/2020/GB'
-// URL = 'http://localhost:4000/publicholidays/2020/GB'
-// URL = 'http://localhost:4000/see-care-php-7.4/staff/calendar/querypage.php'
-
 // URL = 'https://6350235ddf22c2af7b64a794.mockapi.io/api/v1/events';
-URL = 'http://localhost:4000'
-// URL = 'https://see-care-cal-backend.herokuapp.com'
-
+// URL = 'http://localhost:4000'
+URL = 'https://see-care-cal-backend.herokuapp.com'
 
 
 //  #endregion
